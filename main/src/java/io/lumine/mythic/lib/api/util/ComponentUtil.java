87d98a454756c74ac48f6db183b67c34c2286f0a
Ethan <ethan@plexiate.com>
2021-02-12 17:26:28 -0500

    legacy babies cried

--- main/src/java/io/lumine/mythic/lib/api/util/ComponentUtil.java
+++ main/src/java/io/lumine/mythic/lib/api/util/ComponentUtil.java
@@ -2,15 +2,25 @@ package io.lumine.mythic.lib.api.util;
 
 import io.lumine.mythic.lib.MythicLib;
 import io.lumine.utils.text.Component;
+import io.lumine.utils.text.minimessage.MiniMessage;
 import io.lumine.utils.text.serializer.legacy.LegacyComponentSerializer;
 import io.lumine.utils.text.serializer.plain.PlainComponentSerializer;
 
 public class ComponentUtil {
+    private static final LegacyComponentSerializer ampersandRGB = LegacyComponentSerializer.builder().character('&').hexCharacter('#').hexColors().build();
+
     public static Component fromString(String text) {
-        return LegacyComponentSerializer.legacyAmpersand().deserialize(MythicLib.inst().parseColors(text));
+        return ampersandRGB.deserialize(MythicLib.inst().parseColors(text));
     }
 
-    public static String toString(Component component) {
+    public static String toPlainString(Component component) {
         return PlainComponentSerializer.plain().serialize(component);
+    }
+
+    // Fuck everyone who uses legacy color codes for making me do this.
+    // I forgive nobody.
+    public Component fromLegacy(String text) {
+        return MiniMessage.get().deserialize(MiniMessage.get().serialize(
+                ampersandRGB.deserialize(MythicLib.inst().parseColors(text))));
     }
 }
