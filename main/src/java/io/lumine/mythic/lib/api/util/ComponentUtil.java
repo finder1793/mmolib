a0080b21394755c2efe367d273e8a930f0345d5f
Ethan <ethan@plexiate.com>
2021-02-12 18:01:27 -0500

    :(

--- main/src/java/io/lumine/mythic/lib/api/util/ComponentUtil.java
+++ main/src/java/io/lumine/mythic/lib/api/util/ComponentUtil.java
@@ -2,7 +2,6 @@ package io.lumine.mythic.lib.api.util;
 
 import io.lumine.mythic.lib.MythicLib;
 import io.lumine.utils.text.Component;
-import io.lumine.utils.text.minimessage.MiniMessage;
 import io.lumine.utils.text.serializer.legacy.LegacyComponentSerializer;
 import io.lumine.utils.text.serializer.plain.PlainComponentSerializer;
 
@@ -15,12 +14,5 @@ public class ComponentUtil {
 
     public static String toPlainString(Component component) {
         return PlainComponentSerializer.plain().serialize(component);
-    }
-
-    // Fuck everyone who uses legacy color codes for making me do this.
-    // I forgive nobody. :(
-    public Component fromLegacy(String text) {
-        return MiniMessage.get().deserialize(MiniMessage.get().serialize(
-                ampersandRGB.deserialize(MythicLib.inst().parseColors(text))));
     }
 }
