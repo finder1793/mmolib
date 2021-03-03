7740af5d307fe22486676d32c1c5919789ad0159
Ethan <ethan@plexiate.com>
2021-03-03 11:31:56 -0500

    Color fixing requires latest mmoitems.

--- main/src/java/io/lumine/mythic/lib/api/util/LegacyComponent.java
+++ main/src/java/io/lumine/mythic/lib/api/util/LegacyComponent.java
@@ -1,6 +1,7 @@
 package io.lumine.mythic.lib.api.util;
 
 import io.lumine.utils.adventure.text.Component;
+import io.lumine.utils.adventure.text.format.TextDecoration;
 import io.lumine.utils.adventure.text.minimessage.MiniMessage;
 import net.md_5.bungee.api.ChatColor;
 import org.jetbrains.annotations.NotNull;
@@ -9,6 +10,12 @@ public class LegacyComponent {
     private static final String PATTERN = "<(?:#|HEX)([a-fA-F0-9]{6})>";
     @NotNull
     public static Component parse(String text) {
-        return MiniMessage.get().parse(ChatColor.translateAlternateColorCodes('&', text.replaceAll(PATTERN, "<#$1>")));
+        Component component = MiniMessage.get().parse(ChatColor
+                .translateAlternateColorCodes('&', text.replaceAll(PATTERN, "<#$1>")));
+
+        if(component.decorations().get(TextDecoration.ITALIC) == TextDecoration.State.NOT_SET && component.color() != null)
+            component = component.decoration(TextDecoration.ITALIC, TextDecoration.State.FALSE);
+
+        return component;
     }
 }
