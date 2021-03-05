89e87fe7cb658fc2fe1143d55b2083dbed5d4f3e
Ethan <ethan@plexiate.com>
2021-03-05 11:43:49 -0500

    fix hex colors not showing in crafting station display names.

--- main/src/java/io/lumine/mythic/lib/api/util/LegacyComponent.java
+++ main/src/java/io/lumine/mythic/lib/api/util/LegacyComponent.java
@@ -4,15 +4,19 @@ import io.lumine.utils.adventure.text.Component;
 import io.lumine.utils.adventure.text.format.NamedTextColor;
 import io.lumine.utils.adventure.text.format.TextDecoration;
 import io.lumine.utils.adventure.text.minimessage.MiniMessage;
+import io.lumine.utils.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.md_5.bungee.api.ChatColor;
 import org.jetbrains.annotations.NotNull;
 
 public class LegacyComponent {
     private static final String PATTERN = "<(?:#|HEX)([a-fA-F0-9]{6})>";
+    private static final LegacyComponentSerializer unusualSectionRGB = LegacyComponentSerializer.builder().character('§').hexCharacter('#').hexColors()
+            .useUnusualXRepeatedCharacterHexFormat().build();
+
     @NotNull
     public static Component parse(String text) {
-        Component component = MiniMessage.get().parse(ChatColor
-                .translateAlternateColorCodes('&', text.replaceAll(PATTERN, "<#$1>")));
+        Component component = MiniMessage.get().parse(MiniMessage.get().serialize(unusualSectionRGB.
+                deserialize(ChatColor.translateAlternateColorCodes('&', text.replaceAll(PATTERN, "<#$1>")))));
 
         if (component.decorations().get(TextDecoration.ITALIC) == TextDecoration.State.NOT_SET)
             component = component.decoration(TextDecoration.ITALIC, TextDecoration.State.FALSE);
