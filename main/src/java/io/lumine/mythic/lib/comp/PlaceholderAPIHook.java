6263c728ccbaae7be6f06905f1f83ad34e7236ad
Gunging <gungingbross@gmail.com>
2021-03-29 09:33:07 -0500

    Try again...

--- main/src/java/io/lumine/mythic/lib/comp/PlaceholderAPIHook.java
+++ main/src/java/io/lumine/mythic/lib/comp/PlaceholderAPIHook.java
@@ -1,7 +1,7 @@
 package io.lumine.mythic.lib.comp;
 
 import io.lumine.mythic.lib.api.placeholders.MythicPlaceholder;
-import me.clip.placeholderapi.PlaceholderAPI;
+//todo import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.OfflinePlayer;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -19,7 +19,8 @@ public class PlaceholderAPIHook implements MythicPlaceholder {
 
     @NotNull @Override public String getMythicIdentifier() { return ""; }
 
-    @Nullable @Override public String parse(@NotNull String text, @NotNull Object thing) { return PlaceholderAPI.setPlaceholders((OfflinePlayer) thing, text); }
+    @Nullable @Override public String parse(@NotNull String text, @NotNull Object thing) { return ""; }
+    //@Nullable @Override public String parse(@NotNull String text, @NotNull Object thing) { return PlaceholderAPI.setPlaceholders((OfflinePlayer) thing, text); }
 
     @Override public boolean forUseWith(@NotNull Object obj) { return (obj instanceof OfflinePlayer); }
 }
