8dc4e413664ca420b6cb9b5177cd23ff155f727e
Ethan <ethan@plexiate.com>
2021-05-28 20:27:58 -0400

    attempts to fix speed not updating issue

--- main/src/java/io/lumine/mythic/lib/listener/PlayerListener.java
+++ main/src/java/io/lumine/mythic/lib/listener/PlayerListener.java
@@ -1,11 +1,15 @@
 package io.lumine.mythic.lib.listener;
 
+import io.lumine.mythic.lib.MythicLib;
 import io.lumine.mythic.lib.api.player.MMOPlayerData;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.UUID;
 
 public class PlayerListener implements Listener {
 
@@ -14,10 +18,16 @@ public class PlayerListener implements Listener {
         MMOPlayerData.setup(e.getUniqueId());
     }
 
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void playerJoinEvent(PlayerJoinEvent e) {
+        UUID uuid = e.getPlayer().getUniqueId();
+
+        if (MMOPlayerData.isLoaded(uuid))
+            MythicLib.plugin.getStats().runUpdates(MMOPlayerData.get(uuid).getStatMap());
+    }
+
     @EventHandler
     public void b(PlayerQuitEvent event) {
         MMOPlayerData.get(event.getPlayer()).setPlayer(null);
     }
-
-
 }
