7290716c30004d9508fca4f39e2abda95da1b184
Joshua <Joshua@users.noreply.github.com>
2021-08-02 12:09:44 -0500

    temporarily removed all holgorams support to make this build.

--- main/src/java/io/lumine/mythic/lib/comp/holograms/HolographicDisplaysPlugin.java
+++ main/src/java/io/lumine/mythic/lib/comp/holograms/HolographicDisplaysPlugin.java
@@ -1,9 +1,5 @@
 package io.lumine.mythic.lib.comp.holograms;
 
-import com.gmail.filoghost.holographicdisplays.api.Hologram;
-import com.gmail.filoghost.holographicdisplays.api.HologramsAPI;
-import io.lumine.mythic.lib.MythicLib;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
@@ -11,10 +7,10 @@ public class HolographicDisplaysPlugin extends HologramSupport {
 
     @Override
     public void displayIndicator(Location loc, String format, Player player) {
-        Hologram hologram = HologramsAPI.createHologram(MythicLib.plugin, loc);
-        hologram.appendTextLine(format);
-        if (player != null)
-            hologram.getVisibilityManager().hideTo(player);
-        Bukkit.getScheduler().scheduleSyncDelayedTask(MythicLib.plugin, hologram::delete, 20);
+//        Hologram hologram = HologramsAPI.createHologram(MythicLib.plugin, loc);
+//        hologram.appendTextLine(format);
+//        if (player != null)
+//            hologram.getVisibilityManager().hideTo(player);
+//        Bukkit.getScheduler().scheduleSyncDelayedTask(MythicLib.plugin, hologram::delete, 20);
     }
 }
--- main/src/java/io/lumine/mythic/lib/comp/holograms/TrHologramPlugin.java
+++ main/src/java/io/lumine/mythic/lib/comp/holograms/TrHologramPlugin.java
@@ -1,9 +1,5 @@
 package io.lumine.mythic.lib.comp.holograms;
 
-import io.lumine.mythic.lib.MythicLib;
-import me.arasple.mc.trhologram.api.TrHologramAPI;
-import me.arasple.mc.trhologram.module.display.Hologram;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
@@ -16,9 +12,9 @@ public class TrHologramPlugin extends HologramSupport {
 
     @Override
     public void displayIndicator(final Location loc, final String format, final Player player) {
-        Hologram hologram = TrHologramAPI.builder(loc)
-                .append(format)
-                .build();
-        Bukkit.getScheduler().scheduleSyncDelayedTask(MythicLib.plugin, hologram::destroy, 20L);
+//        Hologram hologram = TrHologramAPI.builder(loc)
+//                .append(format)
+//                .build();
+//        Bukkit.getScheduler().scheduleSyncDelayedTask(MythicLib.plugin, hologram::destroy, 20L);
     }
 }
