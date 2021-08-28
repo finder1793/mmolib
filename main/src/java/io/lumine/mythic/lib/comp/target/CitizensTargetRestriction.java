bcb45b2ae9c28469d22dfdfa438a0d20c34f6807
Ashijin <xikage@gmail.com>
2021-08-28 02:45:59 -0600

    ! fuck sentinel

--- main/src/java/io/lumine/mythic/lib/comp/target/CitizensTargetRestriction.java
+++ main/src/java/io/lumine/mythic/lib/comp/target/CitizensTargetRestriction.java
@@ -7,13 +7,13 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 
 public class CitizensTargetRestriction implements TargetRestriction {
-    public static final boolean sentinels = Bukkit.getPluginManager().getPlugin("Sentinel") != null;
+    //public static final boolean sentinels = Bukkit.getPluginManager().getPlugin("Sentinel") != null;
 
     @Override
     public boolean canTarget(Player source, LivingEntity entity, InteractionType interaction) {
         NPC npc = CitizensAPI.getNPCRegistry().getNPC(entity);
 
         // Just fucking cancel everything or it will sent crap ass packet errors
-        return npc == null || (sentinels && npc.hasTrait(org.mcmonkey.sentinel.SentinelTrait.class));
+        return npc == null; // || (sentinels && npc.hasTrait(org.mcmonkey.sentinel.SentinelTrait.class));
     }
 }
