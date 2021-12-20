c500df0264a20d78bf688e9ac6507a9ad6c8dc30
Indyuce <cym.peyrat@yahoo.fr>
2021-12-20 01:02:07 +0100

    new method to fix mmoitems lore updates

--- plugin/src/main/java/io/lumine/mythic/lib/api/util/LegacyComponent.java
+++ plugin/src/main/java/io/lumine/mythic/lib/api/util/LegacyComponent.java
@@ -12,14 +12,25 @@ public class LegacyComponent {
     private static final LegacyComponentSerializer unusualSectionRGB = LegacyComponentSerializer.builder().character("§".toCharArray()[0]).hexCharacter('#').hexColors()
             .useUnusualXRepeatedCharacterHexFormat().build();
 
+    /**
+     * Used to parse text for display names by adding a white color
+     * code by default if no color is added
+     */
     @NotNull
     public static Component parse(String text) {
-        Component component = MiniMessage.get().parse(NestedSerializer.serialize(unusualSectionRGB.
-                deserialize(MythicLib.inst().parseColors(text))));
+        return simpleParse(text).colorIfAbsent(NamedTextColor.WHITE);
+    }
+
+    /**
+     * Used to parse lore lines (does NOT add a white color code)
+     */
+    @NotNull
+    public static Component simpleParse(String text) {
+        Component component = MiniMessage.get().parse(NestedSerializer.serialize(unusualSectionRGB.deserialize(MythicLib.inst().parseColors(text))));
 
         if (component.decorations().get(TextDecoration.ITALIC) == TextDecoration.State.NOT_SET)
             component = component.decoration(TextDecoration.ITALIC, TextDecoration.State.FALSE);
 
-        return component.colorIfAbsent(NamedTextColor.WHITE);
+        return component;
     }
 }
