dad82a25b14e160cf1c388677ea86bc621b7f040
Indyuce <cym.peyrat@yahoo.fr>
2022-01-16 00:24:11 +0100

    !fixed warp ability

--- plugin/src/main/java/io/lumine/mythic/lib/skill/handler/def/Warp.java
+++ plugin/src/main/java/io/lumine/mythic/lib/skill/handler/def/Warp.java
@@ -30,18 +30,15 @@ public class Warp extends SkillHandler<Warp.WarpSkillResult> {
     public void whenCast(WarpSkillResult result, SkillMetadata skillMeta) {
         Player caster = skillMeta.getCaster().getPlayer();
 
-        caster.getWorld().playSound(caster.getPlayer().getLocation(), Sound.BLOCK_END_PORTAL_FRAME_FILL, 1, 2);
-
+        caster.getWorld().playSound(caster.getLocation(), Sound.BLOCK_END_PORTAL_FRAME_FILL, 1, 2);
         Location loc = result.getLocation();
-        loc.setYaw(caster.getPlayer().getLocation().getYaw());
-        loc.setPitch(caster.getPlayer().getLocation().getPitch());
 
-        new ParabolicProjectile(caster.getPlayer().getLocation().add(0, 1, 0), loc.clone().add(0, 1, 0), () -> {
-            if (caster.getPlayer().isOnline() && !caster.getPlayer().isDead()) {
-                caster.getPlayer().teleport(loc);
-                caster.getPlayer().getWorld().spawnParticle(Particle.EXPLOSION_LARGE, caster.getPlayer().getLocation().add(0, 1, 0), 0);
-                caster.getPlayer().getWorld().spawnParticle(Particle.SPELL_INSTANT, caster.getPlayer().getLocation().add(0, 1, 0), 32, 0, 0, 0, .1);
-                caster.getPlayer().getWorld().playSound(caster.getPlayer().getLocation(), VersionSound.ENTITY_ENDERMAN_TELEPORT.toSound(), 1, 1);
+        new ParabolicProjectile(caster.getLocation().add(0, 1, 0), loc.clone().add(0, 1, 0), () -> {
+            if (caster.isOnline() && !caster.isDead()) {
+                caster.teleport(loc);
+                caster.getWorld().spawnParticle(Particle.EXPLOSION_LARGE, caster.getLocation().add(0, 1, 0), 0);
+                caster.getWorld().spawnParticle(Particle.SPELL_INSTANT, caster.getLocation().add(0, 1, 0), 32, 0, 0, 0, .1);
+                caster.getWorld().playSound(caster.getLocation(), VersionSound.ENTITY_ENDERMAN_TELEPORT.toSound(), 1, 1);
             }
         }, 2, Particle.SPELL_INSTANT);
     }
@@ -52,7 +49,8 @@ public class Warp extends SkillHandler<Warp.WarpSkillResult> {
         public WarpSkillResult(SkillMetadata meta) {
             Player caster = meta.getCaster().getPlayer();
             RayTraceResult res = caster.rayTraceBlocks(50, FluidCollisionMode.NEVER);
-            loc = res == null ? null : res.getHitPosition().toLocation(caster.getWorld()).subtract(caster.getEyeLocation());
+            loc = res == null ? null : res.getHitPosition().toLocation(caster.getWorld());
+            loc.setDirection(caster.getEyeLocation().getDirection());
         }
 
         @Override
