1d4cd7b15a8aa03785bd38c92e4d11af4e42d8ba
Indyuce <cym.peyrat@yahoo.fr>
2022-01-15 14:07:35 +0100

    removed unused classes

--- plugin/src/main/java/io/lumine/mythic/lib/player/modifier/StatPlayerModifier.java
+++
@@ -1,8 +0,0 @@
-package io.lumine.mythic.lib.player.modifier;
-
-import io.lumine.mythic.lib.api.stat.modifier.StatModifier;
-
-public class StatPlayerModifier extends  {
-    private final StatModifier modifier;
-    private final String stat, modifierKey;
-}
