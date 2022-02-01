fca56a82f78b056efdb00e7799e1757c28a39258
Jules <peyrat.jules@yahoo.fr>
2022-02-01 17:00:47 +0100

    Removed NONE damage type and allowed for no damage type attacks

--- plugin/src/main/java/io/lumine/mythic/lib/comp/mythicmobs/mechanic/MMODamageMechanic.java
+++ plugin/src/main/java/io/lumine/mythic/lib/comp/mythicmobs/mechanic/MMODamageMechanic.java
@@ -1,10 +1,9 @@
 package io.lumine.mythic.lib.comp.mythicmobs.mechanic;
 
 import io.lumine.mythic.lib.MythicLib;
+import io.lumine.mythic.lib.UtilityMethods;
 import io.lumine.mythic.lib.api.player.EquipmentSlot;
 import io.lumine.mythic.lib.api.player.MMOPlayerData;
-import io.lumine.mythic.lib.api.stat.StatMap;
-import io.lumine.mythic.lib.api.stat.provider.StatProvider;
 import io.lumine.mythic.lib.damage.AttackMetadata;
 import io.lumine.mythic.lib.damage.DamageMetadata;
 import io.lumine.mythic.lib.damage.DamageType;
@@ -19,62 +18,57 @@ import io.lumine.xikage.mythicmobs.skills.placeholders.parsers.PlaceholderDouble
 import io.lumine.xikage.mythicmobs.util.annotations.MythicMechanic;
 import org.bukkit.entity.LivingEntity;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.logging.Level;
-import java.util.stream.Collectors;
-
 @MythicMechanic(
         author = "Indyuce",
         name = "mmodamage",
         aliases = {"mmod"},
-        description = "Deals damage to the target (compatible with MMO)"
+        description = "Deals damage to the target (compatible with MMO plugins)"
 )
 public class MMODamageMechanic extends DamagingMechanic implements ITargetedEntitySkill {
     protected final PlaceholderDouble amount;
+
+    /**
+     * Can be empty if no damage type is registered.
+     * <p>
+     * It IS possible but any attack should be at least physical or magical.
+     * It should also be either a weapon/skill/unarmed attack.
+     */
     protected final DamageType[] types;
 
     public MMODamageMechanic(String line, MythicLineConfig config) {
         super(line, config);
 
         this.amount = PlaceholderDouble.of(config.getString(new String[]{"amount", "a"}, "1", new String[0]));
-        String[] typesFormat = config.getString(new String[]{"type", "t"}, "SKILL,MAGIC", new String[0]).split("\\,");
+        String typesString = config.getString(new String[]{"type", "t"}, null, new String[0]);
+        this.types = typesString == null || typesString.isEmpty() ? new DamageType[0] : toDamageTypeArray(typesString);
+    }
 
-        // Parse damage types
-        ArrayList<DamageType> parsedTypes = new ArrayList<>();
-        for (String unparsedType : typesFormat) {
-            DamageType parsedType;
-            try { parsedType = DamageType.valueOf(unparsedType); } catch (IllegalArgumentException ignored) {
+    private DamageType[] toDamageTypeArray(String typesString) {
+        String[] split = typesString.split("\\,");
+        DamageType[] array = new DamageType[split.length];
 
-                MythicLib.plugin.getLogger().log(Level.WARNING, "Invalid Damage Type " + unparsedType + " for MMODamage Mechanic. ");
-                continue; }
+        for (int i = 0; i < array.length; i++)
+            array[i] = DamageType.valueOf(UtilityMethods.enumName(split[i]));
 
-            // Include
-            parsedTypes.add(parsedType);
-        }
-
-        // Accept those types
-        this.types = parsedTypes.toArray(new DamageType[0]);
+        return array;
     }
 
     @Override
     public boolean castAtEntity(SkillMetadata data, AbstractEntity target) {
 
-        if (!target.isDead() && target.getBukkitEntity() instanceof LivingEntity && !data.getCaster().isUsingDamageSkill() && (!target.isLiving() || !(target.getHealth() <= 0.0D))) {
-
-            double damage = amount.get(data, target) * data.getPower();
-
-            PlayerMetadata caster = data.getVariables().has("MMOStatMap") ? (PlayerMetadata) data.getVariables().get("MMOStatMap").get()
-                    : MMOPlayerData.get(data.getCaster().getEntity().getUniqueId()).getStatMap().cache(EquipmentSlot.MAIN_HAND);
-            AttackMetadata attackMeta = new AttackMetadata(new DamageMetadata(damage, types), caster);
-
-            // Cooler damage types yeah
-            MythicLib.plugin.getDamage().damage(attackMeta, (LivingEntity) target.getBukkitEntity(), !this.preventKnockback, this.preventImmunity);
-
-            MythicLogger.debug(MythicLogger.DebugLevel.MECHANIC, "+ MMODamageMechanic fired for {0} with {1} power", new Object[]{damage, data.getPower()});
-            return true;
-        } else {
+        if (target.isDead() || !(target.getBukkitEntity() instanceof LivingEntity) || data.getCaster().isUsingDamageSkill() || target.getHealth() <= 0)
             return false;
-        }
+
+        double damage = amount.get(data, target) * data.getPower();
+
+        PlayerMetadata caster = data.getVariables().has("MMOStatMap") ? (PlayerMetadata) data.getVariables().get("MMOStatMap").get()
+                : MMOPlayerData.get(data.getCaster().getEntity().getUniqueId()).getStatMap().cache(EquipmentSlot.MAIN_HAND);
+        AttackMetadata attackMeta = new AttackMetadata(new DamageMetadata(damage, types), caster);
+
+        // Cooler damage types yeah
+        MythicLib.plugin.getDamage().damage(attackMeta, (LivingEntity) target.getBukkitEntity(), !this.preventKnockback, this.preventImmunity);
+
+        MythicLogger.debug(MythicLogger.DebugLevel.MECHANIC, "+ MMODamageMechanic fired for {0} with {1} power", new Object[]{damage, data.getPower()});
+        return true;
     }
 }
