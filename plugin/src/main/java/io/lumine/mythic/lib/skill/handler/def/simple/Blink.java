a411168e24141ecedc789dc610e139e4d87c9c0a
Indyuce <cym.peyrat@yahoo.fr>
2022-11-04 10:40:07 +0100

    Fixed an issue with Blink

--- plugin/src/main/java/io/lumine/mythic/lib/skill/handler/def/simple/Blink.java
+++ plugin/src/main/java/io/lumine/mythic/lib/skill/handler/def/simple/Blink.java
@@ -4,9 +4,14 @@ import io.lumine.mythic.lib.skill.SkillMetadata;
 import io.lumine.mythic.lib.skill.handler.SkillHandler;
 import io.lumine.mythic.lib.skill.result.def.SimpleSkillResult;
 import io.lumine.mythic.lib.version.VersionSound;
+import org.bukkit.FluidCollisionMode;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.entity.Player;
+import org.bukkit.util.RayTraceResult;
+import org.bukkit.util.Vector;
+
+import javax.annotation.Nullable;
 
 public class Blink extends SkillHandler<SimpleSkillResult> {
     public Blink() {
@@ -20,17 +25,27 @@ public class Blink extends SkillHandler<SimpleSkillResult> {
         return new SimpleSkillResult();
     }
 
+    private static final double MIN_RANGE = 50;
+
     @Override
     public void whenCast(SimpleSkillResult result, SkillMetadata skillMeta) {
-        Player caster = skillMeta.getCaster().getPlayer();
+        final Player caster = skillMeta.getCaster().getPlayer();
+        final double range = Math.max(MIN_RANGE, skillMeta.getModifier("range"));
+        final Vector dir = caster.getEyeLocation().getDirection();
 
+        // Effects on prev position
         caster.getWorld().spawnParticle(Particle.EXPLOSION_LARGE, caster.getLocation().add(0, 1, 0), 0);
         caster.getWorld().spawnParticle(Particle.SPELL_INSTANT, caster.getLocation().add(0, 1, 0), 32, 0, 0, 0, .1);
         caster.getWorld().playSound(caster.getLocation(), VersionSound.ENTITY_ENDERMAN_TELEPORT.toSound(), 1, 1);
-        Location loc = caster.getTargetBlock(null, (int) skillMeta.getModifier("range")).getLocation().add(0, 1, 0);
+
+        @Nullable final RayTraceResult rtResult = caster.getWorld().rayTraceBlocks(caster.getEyeLocation(), dir, range, FluidCollisionMode.NEVER, true);
+        final Location loc = rtResult == null || rtResult.getHitBlock() == null ? caster.getLocation().add(dir.clone().multiply(range)) :
+                rtResult.getHitPosition().add(rtResult.getHitBlockFace().getDirection().multiply(.3)).toLocation(caster.getWorld());
         loc.setYaw(caster.getLocation().getYaw());
         loc.setPitch(caster.getLocation().getPitch());
         caster.teleport(loc);
+
+        // Effects on newest position
         caster.getWorld().spawnParticle(Particle.EXPLOSION_LARGE, caster.getLocation().add(0, 1, 0), 0);
         caster.getWorld().spawnParticle(Particle.SPELL_INSTANT, caster.getLocation().add(0, 1, 0), 32, 0, 0, 0, .1);
     }
