1421062a6bbaaed136133a305108e587ebacf3b8
Indyuce <cym.peyrat@yahoo.fr>
2022-12-26 01:05:14 +0100

    Removed unused classes

--- plugin/src/main/java/io/lumine/mythic/lib/exception/EmptyWeightListException.java
+++
@@ -1,9 +0,0 @@
-package io.lumine.mythic.lib.exception;
-
-public class EmptyWeightListException extends RuntimeException {
-    private static final long serialVersionUID = -6109415438533989367L;
-
-    public EmptyWeightListException() {
-        super("Couldn't select weighted object; Weighted List is empty.");
-    }
-}
