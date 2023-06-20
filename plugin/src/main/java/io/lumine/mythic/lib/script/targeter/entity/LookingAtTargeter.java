521b6bfa15317e48f63e7fb685679a16e08b2191
Jules <cym.peyrat@yahoo.fr>
2023-06-20 18:46:15 +0200

    Improved 'looking_at'

--- plugin/src/main/java/io/lumine/mythic/lib/script/targeter/entity/LookingAtTargeter.java
+++ plugin/src/main/java/io/lumine/mythic/lib/script/targeter/entity/LookingAtTargeter.java
@@ -2,6 +2,7 @@ package io.lumine.mythic.lib.script.targeter.entity;
 
 import io.lumine.mythic.lib.script.targeter.EntityTargeter;
 import io.lumine.mythic.lib.skill.SkillMetadata;
+import io.lumine.mythic.lib.util.DoubleFormula;
 import io.lumine.mythic.lib.util.configobject.ConfigObject;
 import org.apache.commons.lang.Validate;
 import org.bukkit.FluidCollisionMode;
@@ -17,19 +18,23 @@ import java.util.List;
  * Targets the entity the player is looking at
  */
 public class LookingAtTargeter implements EntityTargeter {
-    private final double range, size;
+    private final DoubleFormula range, size;
     private final boolean ignorePassable;
 
     public LookingAtTargeter(ConfigObject config) {
-        size = config.getDouble("size", .2f);
-        Validate.isTrue(size >= 0, "Size must be positive");
-        range = config.getDouble("length", 50);
-        Validate.isTrue(range > 0, "Length must be strictly positive");
+        size = config.getDoubleFormula("size", new DoubleFormula(.2f));
+        range = config.getDoubleFormula("length", new DoubleFormula(50));
         ignorePassable = config.getBoolean("ignore_passable", true);
     }
 
     @Override
     public List<Entity> findTargets(SkillMetadata meta) {
+        final double size = this.size.evaluate(meta);
+        final double range = this.range.evaluate(meta);
+
+        Validate.isTrue(size >= 0, "Size must be positive");
+        Validate.isTrue(range > 0, "Length must be strictly positive");
+
         Location source = meta.getCaster().getPlayer().getEyeLocation();
         RayTraceResult result = meta.getSourceLocation().getWorld().rayTrace(source, source.getDirection(), range, FluidCollisionMode.NEVER, ignorePassable, size, entity -> true);
         return result == null || result.getHitEntity() == null ? new ArrayList<>() : Arrays.asList(result.getHitEntity());
