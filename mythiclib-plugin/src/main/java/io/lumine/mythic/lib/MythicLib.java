04fc2404458732dbf55bb6a65378289ca47d860d
Jules <cym.peyrat@yahoo.fr>
2023-07-24 21:02:49 +0200

    Fixed NPE

--- mythiclib-plugin/src/main/java/io/lumine/mythic/lib/MythicLib.java
+++ mythiclib-plugin/src/main/java/io/lumine/mythic/lib/MythicLib.java
@@ -373,7 +373,7 @@ public class MythicLib extends JavaPlugin {
     }
 
     public boolean usesProfileId() {
-        return usesProfileId;
+        return usesProfileId != null && usesProfileId.booleanValue();
     }
 
     @Deprecated
