97e89ea368b57329ee0dc6a418b3707cc756f2f4
Jules <cym.peyrat@yahoo.fr>
2023-11-13 00:31:58 +0100

    Fixed crash on PVP

--- mythiclib-plugin/src/main/java/io/lumine/mythic/lib/manager/FakeEventManager.java
+++ mythiclib-plugin/src/main/java/io/lumine/mythic/lib/manager/FakeEventManager.java
@@ -6,10 +6,8 @@ import io.lumine.mythic.lib.api.event.fake.FakeEventCaller;
 import org.apache.commons.lang.Validate;
 import org.bukkit.event.Event;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,7 +15,7 @@ import java.util.List;
 import java.util.Map;
 
 public class FakeEventManager {
-    private final Map<Class, List<FakeEventCaller>> callers = new HashMap<>();
+    private final Map<Class<?>, List<FakeEventCaller>> callers = new HashMap<>();
 
     public FakeEventManager() {
 
@@ -36,27 +34,14 @@ public class FakeEventManager {
     public <E extends Event> void registerFakeEventCaller(Class<E> eventClass, @NotNull FakeEventCaller<E> caller) {
         Validate.notNull(eventClass, "Event class cannot be null");
         Validate.notNull(caller, "Fake event caller cannot be null");
-        final Class fixedClass = fixClass(eventClass);
-        this.callers.computeIfAbsent(fixedClass, unused -> new ArrayList<>()).add(caller);
+        this.callers.computeIfAbsent(eventClass, unused -> new ArrayList<>()).add(caller);
     }
 
     public boolean isFake(@NotNull Event event) {
         Validate.notNull(event, "Event cannot be null");
-        final Class fixedClass = fixClass(event.getClass());
-        final @Nullable List<FakeEventCaller> callers = this.callers.get(fixedClass);
-        if (callers == null) return false;
-
-        for (FakeEventCaller caller : callers) if (caller.isFake(event)) return true;
+        for (Map.Entry<Class<?>, List<FakeEventCaller>> entry : this.callers.entrySet())
+            if (entry.getKey().isInstance(event))
+                for (FakeEventCaller caller : entry.getValue()) if (caller.isFake(event)) return true;
         return false;
-    }
-
-    /**
-     * This takes into account the following simplifications:
-     * - EntityDamageByEntityEvent and EntityDamageEvent are unified
-     */
-    @NotNull
-    private Class<? extends Event> fixClass(@NotNull Class<? extends Event> eventClass) {
-        if (eventClass == EntityDamageByEntityEvent.class) return EntityDamageEvent.class;
-        return eventClass;
     }
 }
